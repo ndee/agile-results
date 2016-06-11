de0176c5d5af5d44654042a7df96f5ebbfe04f6c
andi <henrich.andreas@gmail.com>
2016-06-11 09:37:43 +0200

    Sample authentication.

--- client/imports/daily-log-list/daily-log-list.component.ts
+++ client/imports/daily-log-list/daily-log-list.component.ts
@@ -4,12 +4,13 @@ import { Component } from "@angular/core";
 import { DailyLogCollection } from "../../../collections/daily-log.collection";
 import { Mongo } from "meteor/mongo";
 import { RouterLink } from "@angular/router-deprecated";
+import { LoginButtons } from "angular2-meteor-accounts-ui";
 
 
 @Component( {
     selector: "daily-log-list",
     templateUrl: "/client/imports/daily-log-list/daily-log-list.component.html",
-    directives: [ RouterLink ]
+    directives: [ RouterLink, LoginButtons ]
 } )
 export class DailyLogListComponent {
     logEntries: Mongo.Cursor<DailyLogEntry>;
--- collections/daily-log.collection.ts
+++ collections/daily-log.collection.ts
@@ -1,3 +1,18 @@
-import {Mongo} from "meteor/mongo";
+import { Mongo } from "meteor/mongo";
 
-export let DailyLogCollection = new Mongo.Collection<DailyLogEntry>("dailyLog");
+export let DailyLogCollection = new Mongo.Collection<DailyLogEntry>( "dailyLog" );
+
+DailyLogCollection.allow( {
+    insert: function () {
+        let user = Meteor.user();
+        return !!user;
+    },
+    update: function () {
+        let user = Meteor.user();
+        return !!user;
+    },
+    remove: function () {
+        let user = Meteor.user();
+        return !!user;
+    }
+} );
