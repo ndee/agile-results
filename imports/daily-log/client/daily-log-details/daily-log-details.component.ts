fcab0e293f0372b505df26c716e33eb12c544d34
andi <henrich.andreas@gmail.com>
2016-06-17 19:55:43 +0200

    Details view working again.

--- imports/daily-log/client/daily-log-details/daily-log-details.component.ts
+++ imports/daily-log/client/daily-log-details/daily-log-details.component.ts
@@ -1,23 +1,21 @@
-import { Component, NgZone } from "@angular/core";
+import { Component } from "@angular/core";
+import { MeteorComponent } from "angular2-meteor";
 import { RouteParams } from "@angular/router-deprecated";
-import { Tracker } from "meteor/tracker";
 import { DailyLogCollection } from "../../collections/daily-log.collection";
 
 @Component( {
     selector: "daily-log-details",
-    templateUrl: "/imports/daily-log/client/daily-log-details.component.html"
+    templateUrl: "/imports/daily-log/client/daily-log-details/daily-log-details.component.html"
 } )
-export class DailyLogDetailsComponent {
+export class DailyLogDetailsComponent extends MeteorComponent {
     logEntry: DailyLogEntry;
 
-    constructor( params: RouteParams, ngZone: NgZone ) {
+    constructor( params: RouteParams ) {
+        super();
         let entryId = params.get( "entryId" );
 
-        Tracker.autorun( () => {
-            ngZone.run( () => {
-                this.logEntry = DailyLogCollection.findOne( entryId );
-            } );
-        } );
-
+        this.subscribe( "dailyLog", () => {
+            this.logEntry = DailyLogCollection.findOne( entryId );
+        }, true );
     }
 }
